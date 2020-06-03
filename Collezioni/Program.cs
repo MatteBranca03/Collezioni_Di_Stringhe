f6735a8c2e0a85eb94ec89465f3ffd343797777e
MATTEO BRANCALEONI <mattebranca2003@gmail.com>
2020-06-03 16:55:06 +0200

    Aggiungere i file di progetto.

---
+++ Collezioni/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Collezioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserisci una stringa stringa ");
+            string a = Console.ReadLine();
+            Console.Write("Inserisci una stringa ");
+            string b = Console.ReadLine();
+            bool u = MetodiCollezioni.Uguali(a, b);
+            Console.Write("Le stringhe sono uguali? " + u);
+            Console.WriteLine();
+            Console.Write("Inserisci una stringa ");
+            string c = Console.ReadLine();
+            bool m = MetodiCollezioni.ContainsDoubleChar(c);
+            Console.Write("La stringa contiene lettere maiuscole? " + m) ;
+        }
+    }
+}
---
+++ Metodi/MethodCollezioni.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Method
+{
+    public class MethodCollezioni
+    {
+        public static bool Uguali(string a, string b)
+        {
+            if (a != b)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static bool ContainsDoubleChar(string c)
+        {
+            int m = 0;
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (char.IsUpper(c[i]) == true)
+                    m++;
+            }
+            if (m < 0)
+                return false;
+            else
+                return true;
+        }
+    }
+}
+
